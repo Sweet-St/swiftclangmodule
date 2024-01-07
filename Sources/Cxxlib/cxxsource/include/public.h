9911c8a40ab1e106a6c9812ab8b5ab68baa67033
Fei XU <fei4xu@gmail.com>
2024-01-07 17:08:52 +0800

    Ubunbu can use <angle>

--- Sources/Cxxlib/cxxsource/include/public.h
+++ Sources/Cxxlib/cxxsource/include/public.h
@@ -1,9 +1,9 @@
 //public header for cxxsource
 
 // error: 'sublib2/public2.h' file not found with <angled> include; use "quotes" instead
-// #include <sublib1/public1.h>
-// #include <sublib2/public2.h>
+#include <sublib1/public1.h>
+#include <sublib2/public2.h>
 
 
-#include "sublib1/public1.h"
-#include "sublib2/public2.h"
+// #include "sublib1/public1.h"
+// #include "sublib2/public2.h"
